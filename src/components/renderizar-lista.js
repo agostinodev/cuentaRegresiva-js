21a719e5e4e67d824217a48512a23e6d61dbba61
Nicolas Javier Agostino <nico.agostino@hotmail.com>
2025-09-19 20:54:47 -0300

    Modifica funcion cuentaRegresiva y agrega gitignore

--- src/components/renderizar-lista.js
+++ src/components/renderizar-lista.js
@@ -7,30 +7,33 @@ export function renderizarLista(){
     const $lista = document.querySelector('.main__lista-contenedor');
     const $template = document.querySelector('.template__lista');
 
-    $lista.innerHTML = '';
-
-    console.log(lista);
-
-    lista.forEach( item => {
-
-        const $clone = $template.content.cloneNode(true);
-
-        $clone.querySelector('.item__descripcion').textContent = item.descripcion;
-        const $span = $clone.querySelector('.item__tiempo');
-        const $btnEliminar = $clone.querySelector('.item__btn-eliminar');
-
-        cuentaRegresiva(item.fecha, $span);
 
 
-        $btnEliminar.addEventListener('click', () => {
-            eliminarItem(item.id)
-            renderizarLista()
-        })
+        $lista.innerHTML = ''
+
+        console.log(lista);
+
+        lista.forEach( item => {
+
+            const $clone = $template.content.cloneNode(true);
+
+            $clone.querySelector('.item__descripcion').textContent = item.descripcion;
+            const $span = $clone.querySelector('.item__tiempo');
+            const $btnEliminar = $clone.querySelector('.item__btn-eliminar');
+
+            cuentaRegresiva(item.fecha, $span);
+
+            $btnEliminar.addEventListener('click', () => {
+                eliminarItem(item.id)
+                renderizarLista()
+            })
 
 
-        $lista.appendChild($clone);
+            $lista.appendChild($clone);
+        
+        });
 
-    });
+
 
 
 
--- src/utils/cuenta-regresiva.js
+++ src/utils/cuenta-regresiva.js
@@ -3,43 +3,47 @@
 export function cuentaRegresiva(fechaUsuario, $elemento){
 
     const [anio, mes, dia] = fechaUsuario.split('-').map(Number);
-  
- 
-    const timer = setInterval(() => {
+    
 
-        let ahoraMs = new Date().getTime();
-        let fechaMs = new Date(anio, mes -1, dia).getTime();
+        function actualizarTimer(){
 
-        
+            let ahoraMs = new Date().getTime();
+            let fechaMs = new Date(anio, mes -1, dia).getTime();
 
-        let diferenciaMs = fechaMs - ahoraMs;
-     
-        let tiempo = Math.abs(diferenciaMs);
+            let diferenciaMs = fechaMs - ahoraMs;
+            let tiempo = Math.abs(diferenciaMs);
 
 
-        let dias = Math.floor(tiempo/(1000 * 60 * 60 * 24));
-        let horas = Math.floor(tiempo%(1000 * 60 * 60 * 24)/(1000 * 60 * 60));
-        let minutos = Math.floor(tiempo%(1000 * 60 * 60)/(1000 * 60));
-        let segundos = Math.floor(tiempo%(1000 * 60)/(1000));
+            let dias = Math.floor(tiempo/(1000 * 60 * 60 * 24));
+            let horas = Math.floor(tiempo%(1000 * 60 * 60 * 24)/(1000 * 60 * 60));
+            let minutos = Math.floor(tiempo%(1000 * 60 * 60)/(1000 * 60));
+            let segundos = Math.floor(tiempo%(1000 * 60)/(1000));
 
-        /* console.log(`dias ${dias}  horas ${horas}  minutos ${minutos}  segundos ${segundos} `) */
+            /* console.log(`dias ${dias}  horas ${horas}  minutos ${minutos}  segundos ${segundos} `) */
 
-        if(diferenciaMs === 0){
-            clearInterval(timer)
-            $elemento.textContent = 'El tiempo ya termino.'
-        }
+            if(diferenciaMs === 0){
+                clearInterval()
+                $elemento.textContent = 'El tiempo ya termino.'
+
+            }else if(diferenciaMs < 0){
             
-        if(diferenciaMs < 0){
+                $elemento.textContent = `Pasaron: ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos. `;
 
-            $elemento.textContent = `Pasaron: ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos. `;
+            }else{
 
-        }else{
+                $elemento.textContent = `Faltan: ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos. `;
+            }
 
-            $elemento.textContent = `Faltan: ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos. `;
         }
-        
-        
-    }, 1000);
+
+        actualizarTimer()
+
+
+        setInterval(actualizarTimer, 1000);
+
+    
+
+    
 
 
 }
